6e7fd2b068fefc9ec549586fcac79000b7c3ee2c
LoganMclachlan <47284221+LoganMclachlan@users.noreply.github.com>
2019-08-07 11:44:11 +0100

    Update currancies_avalible.py

--- Currancy converter/currancies_avalible.py
+++ Currancy converter/currancies_avalible.py
@@ -1,15 +1,21 @@
 import json
 from urllib.request import urlopen
 
-# This program list the three letter symbols for each currency the program can use
-
 with urlopen("http://www.floatrates.com/daily/gbp.json") as response:
     source = response.read()
 
 data = json.loads(source)
 
-print("gbp")
-for item in data:
-    print(item)
+currancies = []
 
-var = input("enter any character to exit")
+for item in data:
+    currancies.append(item)
+
+for i in range(0,len(data)):
+    spacing = " "
+    length = 40 - len(data[currancies[i]]["name"])
+    for space in range(0,length):
+        spacing += " "
+    print("Name: " + data[currancies[i]]["name"] + spacing + "Code: " + data[currancies[i]]["code"])
+
+__v = input("")
