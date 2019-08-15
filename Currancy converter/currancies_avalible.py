15056bda9d1443388f83dcde5d47525f45aa3bd2
LoganMclachlan <47284221+LoganMclachlan@users.noreply.github.com>
2019-08-15 09:50:35 +0100

    Update currancies_avalible.py

--- Currancy converter/currancies_avalible.py
+++ Currancy converter/currancies_avalible.py
@@ -7,21 +7,21 @@ try:
     with urlopen("http://www.floatrates.com/daily/gbp.json") as response:
         source = response.read()
     data = json.loads(source)
-    # initialises the list of currancies
-    currancies = []
+    # initialises the list of currencies
+    currencies = []
     # adds the items from exchange rate data to a list
     for item in data:
-        currancies.append(item)
+        currencies.append(item)
 
     # loop for each currancy in list
     for i in range(0,len(data)):
-        # calculates the space between currancy name and code based on name length
+        # calculates the space between currency name and code based on name length
         spacing = " "
-        length = 40 - len(data[currancies[i]]["name"])
+        length = 40 - len(data[currencies[i]]["name"])
         for space in range(0,length):
             spacing += " "
         # outputs the name and code for each currancy
-        print("Name: " + data[currancies[i]]["name"] + spacing + "Code: " + data[currancies[i]]["code"])
+        print("Name: " + data[currencies[i]]["name"] + spacing + "Code: " + data[currencies[i]]["code"])
         
 except urllib.error.URLError:
         error_message = "Could not connect to internet, please try again later"
