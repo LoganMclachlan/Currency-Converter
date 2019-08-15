d26302143a5ad8c872f9cbeb20c77e1c49ec7fc3
LoganMclachlan <47284221+LoganMclachlan@users.noreply.github.com>
2019-08-15 09:39:59 +0100

    Update currancies_avalible.py

--- Currancy converter/currancies_avalible.py
+++ Currancy converter/currancies_avalible.py
@@ -1,28 +1,30 @@
 import json
 from urllib.request import urlopen
+import urllib.error
 
 # gets exchange rate data
-with urlopen("http://www.floatrates.com/daily/gbp.json") as response:
-    source = response.read()
-data = json.loads(source)
+try:
+    with urlopen("http://www.floatrates.com/daily/gbp.json") as response:
+        source = response.read()
+    data = json.loads(source)
+    # initialises the list of currancies
+    currancies = []
+    # adds the items from exchange rate data to a list
+    for item in data:
+        currancies.append(item)
 
-# initialises the list of currancies
-currancies = []
-
-# adds the items from exchange rate data to a list
-for item in data:
-    currancies.append(item)
-
-# loop for each currancy in list
-for i in range(0,len(data)):
-    # calculates the space between currancy name and code based on name length
-    spacing = " "
-    length = 40 - len(data[currancies[i]]["name"])
-    for space in range(0,length):
-        spacing += " "
-
-    # outputs the name and code for each currancy
-    print("Name: " + data[currancies[i]]["name"] + spacing + "Code: " + data[currancies[i]]["code"])
+    # loop for each currancy in list
+    for i in range(0,len(data)):
+        # calculates the space between currancy name and code based on name length
+        spacing = " "
+        length = 40 - len(data[currancies[i]]["name"])
+        for space in range(0,length):
+            spacing += " "
+        # outputs the name and code for each currancy
+        print("Name: " + data[currancies[i]]["name"] + spacing + "Code: " + data[currancies[i]]["code"])
+        
+except urllib.error.URLError:
+        error_message = "Could not connect to internet, please try again later"
 
 # holds open terminal
 __v = input("")
